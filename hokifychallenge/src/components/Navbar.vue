f9f8c4e0f5f72e4173f90b2e0bde75287d1fc69d
loveeyk <karir.loveleen@gmail.com>
2021-06-08 01:07:04 +0200

    created Navbar component with placeholders

---
+++ hokifychallenge/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+
+  <ul>
+      <li v-for="(link,index) in links" :key="index">
+          {{link}}
+          </li>
+</ul>
+</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+export default class Navbar extends Vue {
+
+links:any = [""];
+
+}
+</script>
+
+<style>
+
+</style>
