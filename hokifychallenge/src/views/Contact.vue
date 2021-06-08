fb50b9a05a8a639141a3393b7341247a2bf423c0
loveeyk <karir.loveleen@gmail.com>
2021-06-09 00:37:23 +0200

    added form

--- hokifychallenge/src/views/Contact.vue
+++ hokifychallenge/src/views/Contact.vue
@@ -1,25 +1,56 @@
 <template>
-  <div class="contact">
-      <span>Contact</span>
+    <div class="form">  
+    <!-- <span>Homeview</span>   -->
+    <label>Vorname: </label>
+    <input v-model="text" placeholder="Vorname">
+     <label>Nachname: </label>
+    <input v-model="text" placeholder="Nachname">
+     <label>Email: </label>
+    <input v-model="email" placeholder="Email">
+    <button @click="openWindow">Senden</button>
+ 
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { Vue } from "vue-class-component";
 
 export default class Contact extends Vue{
 
+openWindow(){
+  alert("Hallo Loveleen Karir bei Hokify")
+
+  return 1
 }
+}
+
 </script>
 
 <style lang="scss">
-.contact{
+.form{
 display: flex;
+flex-direction: column;
 justify-content: center;
 align-items:center;
 height: 100vh;
-  span{
-    font-size: 20vw;
-  }
+
+label{
+  padding-top: 1rem;
+}
+input{
+  padding: 0.5rem;
+  width: 300px;
+}
+  button{
+    background: #027f7e;
+    color: white;
+    padding: 1rem;
+    border-radius: 5px;
+    cursor: pointer;
+     border: 1px solid transparent;
+    
+  }button:hover{
+      background: #10b0af;
+    }
 }
 </style>
