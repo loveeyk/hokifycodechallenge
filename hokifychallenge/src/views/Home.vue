0025dfb4716cf79e5b8c77b7ae670f41988369df
loveeyk <karir.loveleen@gmail.com>
2021-06-09 23:01:39 +0200

    button style edit

--- hokifychallenge/src/views/Home.vue
+++ hokifychallenge/src/views/Home.vue
@@ -3,13 +3,12 @@
   <div class="home">  
    <h1>Willkommen auf Lovify.</h1>
      <!-- <img src="../assets/logo_lovify.svg" alt="logo" /> -->
-  <button type="button">Contact me</button>
+  <a href="contact">Click Me</a>
 
   </div>
 </template>
 
 <script lang="ts">
-import HeroVue from '@/components/Hero.vue';
 import { Options, Vue } from 'vue-class-component';
 
 @Options({
@@ -17,6 +16,8 @@ import { Options, Vue } from 'vue-class-component';
   },
 })
 export default class Home extends Vue {
+
+   
    
 }
 </script>
@@ -28,17 +29,14 @@ export default class Home extends Vue {
   flex-direction: column;
   align-items: center;
   height: 70vh;
-  
-  button{
-    background: #027f7e;
-    color: white;
+  a{
     padding: 1rem;
+    background: #10b0af;
+    color: white;
+    text-decoration: none;
+    border: 1px solid #cafcfc;
+    box-shadow: 1px 2px 8px #96ffff;
     border-radius: 5px;
-    cursor: pointer;
-    border: 1px solid transparent;
-  }button:hover{
-      background: #10b0af;
-    }
-
+  }
 }
 </style>
