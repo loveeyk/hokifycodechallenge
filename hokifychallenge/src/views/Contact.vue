21febd03572ea9bdb60a850e5e907523eae337d3
loveeyk <karir.loveleen@gmail.com>
2021-06-10 00:41:58 +0200

    form valiation and error handling

--- hokifychallenge/src/views/Contact.vue
+++ hokifychallenge/src/views/Contact.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="form">  
+    <form class="form" @submit.prevent="validateForm" method="post">  
       <img src="../assets/logo_lovify.svg"/>
+      <p v-for="(error,i) in errors" :key="i">{{error}}</p>
     <!-- <span>Homeview</span>   -->
     <label>Vorname: </label>
     <input v-model="vname" placeholder="Vorname">
@@ -8,9 +9,8 @@
     <input v-model="nname" placeholder="Nachname">
      <label>Email: </label>
     <input v-model="email" placeholder="Email">
-    <button @click="openWindow">Senden</button>
- 
-  </div>
+    <input type="submit" value="submit"/>
+  </form>
 </template>
 
 <script lang="ts">
@@ -21,8 +21,40 @@ export default class Contact extends Vue{
 vname = ""
 nname = ""
 email = ""
+errors:Array<string> = []
 
-  openWindow():void{
+validateForm(e:any){
+  this.errors = [] //reset
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+  if(this.vname && this.nname && this.email) this.showAlert()
+
+  if(!this.vname) this.errors.push(this.errorString("vname"))  
+  if(!this.nname) this.errors.push(this.errorString("nname"))
+  if(!this.email) this.errors.push(this.errorString("email"))
+  if(this.email && !re.test(this.email)) this.errors.push(this.errorString("emailvalid"))
+}
+
+/** 
+ * return correct error message 
+ **/
+errorString(field:string):string{
+
+  let toReturn = ""
+  switch(field){
+    case "vname": toReturn = "einen Vornamen"
+    break
+    case "nname": toReturn = "einen Nachnamen"
+    break;
+    case "email": toReturn = "eine E-Mail Adresse"
+    break;
+    case "emailvalid": toReturn = "eine gültige E-Mail Adresse"
+  }
+
+  return "Bitte geben Sie " + toReturn + " an!"
+}
+
+showAlert():void{
   alert("Hallo " + this.vname + " " + this.nname + " bei Hokify!");
 }
 }
