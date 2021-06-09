8ba1b1bf0d3bcb9ba6644a21da53fbd1ab2fbf68
loveeyk <karir.loveleen@gmail.com>
2021-06-09 22:59:27 +0200

    responsiveness improved

--- hokifychallenge/src/components/Navbar.vue
+++ hokifychallenge/src/components/Navbar.vue
@@ -1,6 +1,10 @@
 <template>
+
   <ul class="navbar">
+   <li><a href="/"><img src="../assets/logo_lovify.svg"/></a></li>
       <li v-for="(link,index) in links" :key="index">
+        
+          
           <router-link :to="link.path">{{link.name}}</router-link>
     </li>
 </ul>
@@ -11,7 +15,8 @@ import { Options, Vue } from 'vue-class-component'
 
 export default class Navbar extends Vue {
 
-links:any = [{name: "Home", path:"/"}, {name: "Contact", path: "/contact"}, {name: "LoginForm", path: "/login"}];
+links:any = [{name: "Home", path:"/"}, {name: "Contact", path: "/contact"}];
+
 
 }
 </script>
@@ -19,16 +24,32 @@ links:any = [{name: "Home", path:"/"}, {name: "Contact", path: "/contact"}, {nam
 <style lang="scss">
 
 .navbar{
+    display: flex;
     background: #10b0af;
     color: white;
     list-style: none;
+    box-shadow: 1px 3px 15px #54ffff;
+    padding: 0.5rem;
+    width: 100%;
+
+    img{
+        
+        width: 100px;
+    }
     li{ 
-        display: inline-block;
-        padding: 1rem;
+    display: inline-flex;
+    align-items: center;
+    
+    padding: 0 1rem;
+    }
+
+        
         a{
         color: white;
         text-decoration: none;
+    
         }
+       
     }
-}
+
 </style>
