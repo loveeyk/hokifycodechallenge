5e9128ec24c26c8fb4a0545d870de7aafcd12680
loveeyk <karir.loveleen@gmail.com>
2021-06-10 14:42:02 +0200

    .

--- hokifychallenge/src/main.ts
+++ hokifychallenge/src/main.ts
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/global.scss'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+const app = createApp(App)
+
+app.use(VueSweetalert2);
 
 
 createApp(App).use(router).mount('#app')
