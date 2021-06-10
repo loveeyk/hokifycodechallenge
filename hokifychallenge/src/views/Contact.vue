c4859bdf28aecdacc6566fd7a0a351b7df9e5208
loveeyk <karir.loveleen@gmail.com>
2021-06-10 10:59:25 +0200

    added comments

--- hokifychallenge/src/views/Contact.vue
+++ hokifychallenge/src/views/Contact.vue
@@ -20,7 +20,9 @@ export default class Contact extends Vue {
     nname = ""
     email = ""
     errors: Array < string > = []
-
+/**
+ * check form input validation
+ */
     validateForm(e: any) {
         this.errors = [] //reset
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
