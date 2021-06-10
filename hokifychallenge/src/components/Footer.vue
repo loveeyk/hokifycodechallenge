b77e69ef57179525beb5e488b4992c4f57c55a48
loveeyk <karir.loveleen@gmail.com>
2021-06-10 14:42:47 +0200

    created footer component for icons

---
+++ hokifychallenge/src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="footer">
+    <Icons />
+</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Icons from '../components/Icons.vue';
+
+@Options({
+    components: {
+        Icons,
+    },
+})
+export default class Footer extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+@import 'src/assets/colors.scss';
+
+.footer {
+    position: absolute;
+    bottom: 0;
+    background: $color;
+    color: $accent_colorLight;
+    padding: 0.5rem;
+    width: 100%;
+}
+</style>
