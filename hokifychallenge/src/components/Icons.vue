b861c6084d36454fa50953041d3a825315a114a4
loveeyk <karir.loveleen@gmail.com>
2021-06-10 14:43:04 +0200

    positioning changed of icons, text alignment

--- hokifychallenge/src/components/Icons.vue
+++ hokifychallenge/src/components/Icons.vue
@@ -1,14 +1,13 @@
 <template>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
 
-  <div class="icons">
-<i class="fab fa-instagram"></i>
-<i class="fab fa-facebook"></i>
-<i class="fab fa-linkedin"></i>
-<i class="fab fa-github"></i>
-<i class="fab fa-snapchat"></i>
-  </div>
-
+<div class="icons">
+    <i class="fab fa-instagram"></i>
+    <i class="fab fa-facebook"></i>
+    <i class="fab fa-linkedin"></i>
+    <i class="fab fa-github"></i>
+    <i class="fab fa-snapchat"></i>
+</div>
 </template>
 
 <script lang="ts">
@@ -17,19 +16,13 @@ import { Options, Vue } from 'vue-class-component';
 @Options({
     components: {},
 })
-export default class Icons extends Vue {
-    
-}
-
+export default class Icons extends Vue {}
 </script>
 
 <style lang="scss">
-.icons{
-    display: flex;
-
-    align-items: center;
-    margin-left: auto;
-    i{
+.icons {
+    text-align: center;
+    i {
         font-size: 1.5;
         padding: 0 0.5rem;
     }
