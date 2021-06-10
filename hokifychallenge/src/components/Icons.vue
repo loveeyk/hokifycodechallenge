8c3f8673cbe30564fd67219525b41ae70cf741a7
loveeyk <karir.loveleen@gmail.com>
2021-06-10 12:05:04 +0200

    created icons component

---
+++ hokifychallenge/src/components/Icons.vue
@@ -0,0 +1,28 @@
+<template>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+
+  <div class="icons">
+<i class="fab fa-instagram"></i>
+<i class="fab fa-facebook"></i>
+<i class="fab fa-linkedin"></i>
+<i class="fab fa-github"></i>
+<i class="fab fa-snapchat"></i>
+  </div>
+
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    components: {},
+})
+export default class Icons extends Vue {
+    
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
